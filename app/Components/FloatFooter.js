b81fe2975dc726b48722a1b6e896291578ad3b9b
Daniel777y <yudexu123@126.com>
2024-07-18 23:47:04 -0700

    ticket footer

--- app/Components/FloatFooter.js
+++ app/Components/FloatFooter.js
@@ -2,7 +2,8 @@ import TicketBtn from "./TicketBtn";
 
 const FloatFooter = () => {
   return (
-    <div className="float-footer">
+    <div className="fixed bottom-0 w-full bg-white p-4 shadow-lg md:hidden flex justify-between items-center">
+      <span className="font-bold">$179 – $9,999</span>
       <TicketBtn />
     </div>
   );
