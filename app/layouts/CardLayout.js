abd614f82342da768e3abb4a5b22ebecebdd8624
Daniel777y <yudexu123@126.com>
2024-07-19 14:56:31 -0700

    adjust section margin

--- app/layouts/CardLayout.js
+++
@@ -1,9 +0,0 @@
-const CardLayout = ({ children }) => {
-  return (
-    <div className="w-full h-full bg-white shadow-md rounded-lg p-4">
-      {children}
-    </div>
-  );
-};
-
-export default CardLayout;
