a6aa60788aaae5036ef5cbc8301c7a8e7d124b41
Daniel777y <yudexu123@126.com>
2024-07-21 14:59:29 -0700

    update todo & get rid of customized styles

--- app/Layouts/SectionLayout.js
+++ app/Layouts/SectionLayout.js
@@ -1,15 +1,9 @@
-import styles from "../styles/section.module.css";
-
 const SectionLayout = ({ title, description, children }) => {
   return (
-    <div className={`${styles.container} text-left mt-6 mb-6`}>
+    <div className="text-left mt-6 mb-6">
       <div className="bg-sky-400 w-10 h-1 mt-2" />
-      <div className={`${styles.title} text-3xl font-bold mt-2`}>{title}</div>
-      {description && (
-        <div className={`${styles.description} text-sky-400 mt-1`}>
-          {description}
-        </div>
-      )}
+      <div className="text-3xl font-bold mt-2">{title}</div>
+      {description && <div className="text-sky-400 mt-1">{description}</div>}
       <div className="flex flex-col mt-4 items-center justify-center gap-y-4 w-full h-full">
         {children}
       </div>
