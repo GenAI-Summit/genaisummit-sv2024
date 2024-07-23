b0f25bb1420e6450a28eb0ba6baec7bb9a76f8a2
Daniel777y <yudexu123@126.com>
2024-07-23 15:26:12 -0700

    update icons & styles

--- app/Components/FloatFooter.js
+++ app/Components/FloatFooter.js
@@ -2,7 +2,7 @@ import TicketBtn from "./TicketBtn";
 
 const FloatFooter = () => {
   return (
-    <div className="fixed bottom-0 w-full bg-color5 text-color7 p-4 shadow-lg md:hidden flex justify-between items-center z-40">
+    <div className="fixed bottom-0 w-full bg-color5 text-color7 p-4 shadow-lg md:hidden flex justify-between items-center z-40 border-t-2 border-color1">
       <span className="font-bold">$179 – $9,999</span>
       <TicketBtn />
     </div>
--- app/Components/NavBar.js
+++ app/Components/NavBar.js
@@ -33,7 +33,7 @@ const NavBar = () => {
 
   return (
     <>
-      <div className="fixed top-0 px-4 py-2 w-full bg-color5 text-color7 font-semibold flex flex-row justify-between items-center hidden md:flex z-40">
+      <div className="fixed top-0 px-4 py-2 w-full bg-color5 text-color7 font-semibold flex flex-row justify-between items-center hidden md:flex z-40 border-b-2 border-color1">
         <div className="cursor-pointer" onClick={onClickLogo}>
           <Icon
             src="/images/icons/gptdao.png"
@@ -46,7 +46,7 @@ const NavBar = () => {
         <TicketBtn />
       </div>
 
-      <div className="fixed top-0 px-4 py-2 w-full bg-color5 text-color7 font-semibold flex flex-col items-center text-center md:hidden z-40">
+      <div className="fixed top-0 px-4 py-2 w-full bg-color5 text-color7 font-semibold flex flex-col items-center text-center md:hidden z-40 border-b-2 border-color1">
         <div className="w-full flex flex-row justify-between">
           <div className="cursor-pointer" onClick={onClickLogo}>
             <Icon
@@ -57,7 +57,7 @@ const NavBar = () => {
             />
           </div>
           <button
-            className="cursor-pointer hover:bg-gray-200 hover:rounded-lg duration-300"
+            className="cursor-pointer hover:bg-color3 hover:rounded-lg duration-300"
             onClick={handleClickDrawer}
           >
             <Icon
