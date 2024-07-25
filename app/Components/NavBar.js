f8b15c3745bf73b3074bed2350551f72ffa90810
Daniel777y <yudexu123@126.com>
2024-07-25 13:40:58 -0700

    update styles

--- app/Components/NavBar.js
+++ app/Components/NavBar.js
@@ -33,7 +33,7 @@ const NavBar = () => {
 
   return (
     <>
-      <div className="fixed top-0 px-4 py-2 w-full bg-color5 text-color7 font-semibold flex flex-row justify-between items-center hidden md:flex z-40 border-b-2 border-color1">
+      <div className="fixed top-0 px-4 py-2 w-full bg-color5 text-color7 font-semibold flex flex-row justify-around items-center hidden md:flex z-40 border-b-2 border-color1">
         <div className="cursor-pointer" onClick={onClickLogo}>
           <Icon
             src="/images/icons/gptdao.png"
