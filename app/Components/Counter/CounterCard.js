32b2e84b2f2e7908bb8af4d49264621db9351ee3
Daniel777y <yudexu123@126.com>
2024-07-30 12:45:02 -0700

    improve fun facts styles

--- app/Components/Counter/CounterCard.js
+++ app/Components/Counter/CounterCard.js
@@ -4,7 +4,7 @@ import NumberRoller from "./NumberRoller";
 const CounterCard = ({ title, start, end }) => {
   return (
     <CardLayout>
-      <div className="text-2xl md:text-4xl text-color2 font-bold">
+      <div className="text-2xl md:text-4xl text-color1 font-bold">
         <NumberRoller start={start} end={end} />
         <div className="text-lg md:text-xl text-color5 font-semibold">
           {title}
