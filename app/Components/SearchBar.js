45be96910d23f59718379bc23b0d9da14c80342f
Daniel777y <yudexu123@126.com>
2024-07-31 23:32:18 -0700

    update todo & improve search bar

--- app/Components/SearchBar.js
+++ app/Components/SearchBar.js
@@ -1,9 +1,13 @@
 const SearchBar = ({ onChange }) => {
   return (
-    <>
-      <label htmlFor="search">Search: </label>
-      <input type="text" onChange={onChange} maxLength="30" />
-    </>
+    <div className="flex justify-center p-4">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="w-full md:w-[80%] p-2 border-none rounded-full shadow focus:outline-none"
+        onChange={onChange}
+      />
+    </div>
   );
 };
 
