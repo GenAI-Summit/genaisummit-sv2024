6dbb92da94bc698264ba662f89ab7159890dbdc5
Daniel777y <yudexu123@126.com>
2024-07-31 18:29:01 -0700

    api updates

--- app/agenda/page.js
+++ app/agenda/page.js
@@ -6,23 +6,15 @@ import Agenda from "../Components/Agenda";
 import { getSessions } from "/lib/api";
 
 const AgendaPage = () => {
-  const [sessionGroups, setSessionGroups] = useState([]);
-  const [filteredSessionGroups, setFilteredSessionGroups] = useState([]);
+  const [sessions, setSessions] = useState([]);
+  const [filteredSessions, setFilteredSessions] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getSessionsData = async () => {
       const data = await getSessions();
-      const groupedSessions = data.reduce((acc, session) => {
-        if (!acc[session.date]) {
-          acc[session.date] = [];
-        }
-        acc[session.date].push(session);
-        return acc;
-      }, {});
-      const groups = Object.values(groupedSessions);
-      setSessionGroups(groups);
-      setFilteredSessionGroups(groups);
+      setSessions(data);
+      setFilteredSessions(data);
       setLoading(false);
     };
     getSessionsData();
@@ -30,17 +22,18 @@ const AgendaPage = () => {
 
   const onChange = (e) => {
     const text = e.target.value.toLowerCase();
-    const sessions = sessionGroups.map((group) => {
-      return group.filter((session) => {
-        return (
-          session.topic.toLowerCase().includes(text) ||
-          session.speakers.some((speaker) =>
-            speaker.name.toLowerCase().includes(text),
-          )
-        );
-      });
+    const filteredSessions = sessions.filter((session) => {
+      return (
+        session.title.toLowerCase().includes(text) ||
+        session.speakers.some((speaker) =>
+          speaker.name.toLowerCase().includes(text),
+        ) ||
+        session.moderators.some((moderator) =>
+          moderator.name.toLowerCase().includes(text),
+        )
+      );
     });
-    setFilteredSessionGroups(sessions);
+    setFilteredSessions(filteredSessions);
   };
 
   return (
@@ -49,11 +42,7 @@ const AgendaPage = () => {
         <SearchBar onChange={onChange} />
       </div>
       <div className="w-full">
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <Agenda sessionGroups={filteredSessionGroups} />
-        )}
+        {loading ? <p>Loading...</p> : <Agenda sessions={filteredSessions} />}
       </div>
     </div>
   );
