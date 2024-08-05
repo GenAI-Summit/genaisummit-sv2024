f4a6acec28f0b19606ea3ae37fd120fad3e406c3
Daniel777y <yudexu123@126.com>
2024-08-04 19:52:54 -0700

    update api url

--- app/Hooks/useSpeakers.js
+++ app/Hooks/useSpeakers.js
@@ -4,7 +4,7 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const useSpeakers = () => {
   const { data, isLoading, error } = useSWR(
-    "https://api.gptdao.ai/speakers",
+    `${process.env.NEXT_PUBLIC_API_URL}/speakers`,
     fetcher,
   );
 
