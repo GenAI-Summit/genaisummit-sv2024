91e81876695d602b8be13a556c715ac2c0710727
Daniel777y <yudexu123@126.com>
2024-08-05 22:39:46 -0700

    share bar

---
+++ app/Components/ShareBar.js
@@ -0,0 +1,55 @@
+"use client";
+
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  RedditIcon,
+  RedditShareButton,
+  TelegramIcon,
+  TelegramShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+  XIcon,
+  TwitterShareButton,
+} from "react-share";
+
+import { usePathname } from "next/navigation";
+
+const ShareBar = () => {
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}${usePathname()}`;
+  return (
+    <div>
+      <div className="bg-color1 w-10 h-1 mt-2" />
+      <div className="flex justify-center gap-2 items-center">
+        <div className="text-color5 font-bold">Share this page:</div>
+        <TwitterShareButton url={url}>
+          <XIcon size={32} round={true} />
+        </TwitterShareButton>
+        <LinkedinShareButton url={url}>
+          <LinkedinIcon size={32} round={true} />
+        </LinkedinShareButton>
+        <FacebookShareButton url={url}>
+          <FacebookIcon size={32} round={true} />
+        </FacebookShareButton>
+        <RedditShareButton url={url}>
+          <RedditIcon size={32} round={true} />
+        </RedditShareButton>
+        <TelegramShareButton url={url}>
+          <TelegramIcon size={32} round={true} />
+        </TelegramShareButton>
+        <WhatsappShareButton url={url}>
+          <WhatsappIcon size={32} round={true} />
+        </WhatsappShareButton>
+        <EmailShareButton url={url}>
+          <EmailIcon size={32} round={true} />
+        </EmailShareButton>
+      </div>
+    </div>
+  );
+};
+
+export default ShareBar;
