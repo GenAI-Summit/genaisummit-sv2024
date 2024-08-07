fbf8fa024ca18c42ec46d498f47149dbbf2ad570
Daniel777y <yudexu123@126.com>
2024-08-06 18:29:40 -0700

    update reset button style

--- app/Components/ResetBtn.js
+++ app/Components/ResetBtn.js
@@ -1,7 +1,7 @@
 const ResetBtn = ({ onReset }) => {
   return (
     <button
-      className="bg-color1 text-color7 p-2 rounded-lg hover:bg-color2 ease-in-out duration-300"
+      className="bg-color1 text-color7 font-bold py-2 px-4 rounded-lg hover:bg-color2 ease-in-out duration-300"
       onClick={onReset}
     >
       Reset
