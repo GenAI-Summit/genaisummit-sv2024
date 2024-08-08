78f4bd9870e158943f754561dc873e90c7cfdae9
Daniel777y <yudexu123@126.com>
2024-08-08 01:32:53 -0700

    fix: images do not change when zooming in

--- app/Components/Carousel/IntroCarousel.js
+++ app/Components/Carousel/IntroCarousel.js
@@ -1,21 +1,29 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import Zoom from "react-medium-image-zoom";
+import { useState, useEffect, useCallback } from "react";
+import { Controlled as ControlledZoom } from "react-medium-image-zoom";
 import "../../styles/zoom.css";
 import Image from "next/image";
 
 const IntroCarousel = ({ images }) => {
   const [index, setIndex] = useState(0);
+  const [isZoomed, setIsZoomed] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((index) => (index + 1) % images.length);
-    }, 5000);
-    return () => clearInterval(interval);
-  }, [images.length]);
+    if (!isZoomed) {
+      const interval = setInterval(() => {
+        setIndex((index) => (index + 1) % images.length);
+      }, 5000);
+      return () => clearInterval(interval);
+    }
+  }, [isZoomed, images.length]);
+
+  const handleZoomChange = useCallback((shouldZoom) => {
+    setIsZoomed(shouldZoom);
+  }, []);
+
   return (
-    <Zoom>
+    <ControlledZoom isZoomed={isZoomed} onZoomChange={handleZoomChange}>
       <Image
         src={images[index]}
         alt="intro-carousel"
@@ -24,7 +32,7 @@ const IntroCarousel = ({ images }) => {
         className="rounded-lg"
         loading="lazy"
       />
-    </Zoom>
+    </ControlledZoom>
   );
 };
 
