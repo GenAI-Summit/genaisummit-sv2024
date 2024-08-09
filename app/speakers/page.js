b9e20b02538dcd42fb231a8f405ad10325f76c72
Daniel777y <yudexu123@126.com>
2024-08-08 18:14:02 -0700

    feat: add animations while sections enter

--- app/speakers/page.js
+++ app/speakers/page.js
@@ -8,6 +8,7 @@ import Speakers from "../Components/Speakers";
 import ResetBtn from "../Components/ResetBtn";
 import useSpeakers from "../Hooks/useSpeakers";
 import SpeakersIntro from "../Components/Intro/SpeakersIntro";
+import SectionEnter from "../Motions/SectionEnter";
 
 const SpeakersPage = () => {
   const { speakers, isLoading, isError } = useSpeakers();
@@ -37,18 +38,22 @@ const SpeakersPage = () => {
 
   return (
     <div className="w-full flex flex-col gap-y-8">
-      <SpeakersIntro />
-      <div className="w-full flex flex-col md:flex-row gap-y-4 md:gap-x-8">
-        <div className="w-full md:w-2/5 flex justify-center md:max-w-96">
-          <div className="w-full flex flex-col gap-y-4">
-            <SearchBar text={text} setText={setText} />
-            <ResetBtn onReset={onReset} />
+      <SectionEnter>
+        <SpeakersIntro />
+      </SectionEnter>
+      <SectionEnter>
+        <div className="w-full flex flex-col md:flex-row gap-y-4 md:gap-x-8">
+          <div className="w-full md:w-2/5 flex justify-center md:max-w-96">
+            <div className="w-full flex flex-col gap-y-4">
+              <SearchBar text={text} setText={setText} />
+              <ResetBtn onReset={onReset} />
+            </div>
+          </div>
+          <div className="w-full">
+            <Speakers speakers={filteredSpeakers} />
           </div>
         </div>
-        <div className="w-full">
-          <Speakers speakers={filteredSpeakers} />
-        </div>
-      </div>
+      </SectionEnter>
     </div>
   );
 };
