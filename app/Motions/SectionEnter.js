e618eaeee769c5b62ed944d20fbe9be58cb221a0
Daniel777y <yudexu123@126.com>
2024-08-08 18:18:16 -0700

    feat: add animations while sections enter

--- app/Motions/SectionEnter.js
+++ app/Motions/SectionEnter.js
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 
 const SectionEnter = ({ children }) => (
   <motion.div
+    className="w-full h-full"
     initial={{
       opacity: 0,
       x: Math.floor(Math.random() * 2) === 0 ? -100 : 100,
