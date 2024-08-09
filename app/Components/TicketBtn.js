716d977073cf736733f7f341bcbc3a56b031ce4a
Daniel777y <yudexu123@126.com>
2024-08-09 15:18:03 -0700

    refactor: update ticket button style

--- app/Components/TicketBtn.js
+++ app/Components/TicketBtn.js
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 const TicketBtn = () => {
   return (
-    <button className="bg-color1 hover:bg-color2 text-color7 font-bold py-2 px-4 rounded ease-in-out duration-300">
-      <Link
-        href="https://whova.com/portal/registration/genai_202405/"
-        target="_blank"
-      >
+    <Link
+      href="https://whova.com/portal/registration/genai_202405/"
+      target="_blank"
+    >
+      <button className="bg-color1 hover:bg-color2 text-color7 font-bold py-2 px-4 rounded ease-in-out duration-300">
         Buy Tickets
-      </Link>
-    </button>
+      </button>
+    </Link>
   );
 };
 
