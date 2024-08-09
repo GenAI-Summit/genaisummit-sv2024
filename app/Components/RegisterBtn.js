46f1cc2cf4c9a5f4e70f1cd626626c797f8c1e50
Daniel777y <yudexu123@126.com>
2024-08-08 17:21:35 -0700

    feat: add popup registeration form

--- app/Components/RegisterBtn.js
+++ app/Components/RegisterBtn.js
@@ -1,6 +1,12 @@
 const RegisterBtn = ({ onClick, text = "Register" }) => {
   return (
     <button
+      data-tally-layout="modal"
+      data-tally-open="nPl9xe"
+      data-tally-width="700"
+      data-tally-emoji-text="👋"
+      data-tally-emoji-animation="wave"
+      data-tally-auto-close="3000"
       className="bg-color1 hover:bg-color2 text-color7 py-2 px-4 font-bold rounded ease-in-out duration-300"
       onClick={onClick}
     >
