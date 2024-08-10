fe48f333477e1a0fd787de2fe4837f3890969b09
Daniel777y <yudexu123@126.com>
2024-08-09 22:27:44 -0700

    chore: remove html parser

--- app/Components/OrganizationDetail.js
+++ app/Components/OrganizationDetail.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Logo from "./Logo";
 
-import parse from "html-react-parser";
+// import parse from "html-react-parser";
 
 const OrganizationDetail = ({ organization }) => {
   return (
@@ -15,7 +15,7 @@ const OrganizationDetail = ({ organization }) => {
       </Link>
       <div className="mt-4 text-2xl font-bold">{organization.name}</div>
       <div className="mt-2 text-left text-lg md:text-xl">
-        {parse(organization.desc)}
+        {organization.desc}
       </div>
     </div>
   );
