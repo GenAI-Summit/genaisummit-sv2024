e2cb5ea94633bc39cf29ebaa532fb20e5379c9fe
Daniel777y <yudexu123@126.com>
2024-08-11 09:44:08 -0700

    feat: filter animations

--- app/Components/SVG/WrapperArrow.js
+++ app/Components/SVG/WrapperArrow.js
@@ -10,7 +10,7 @@ const WrapperArrow = ({ isOpen }) => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="M15 18L9 12L15 6"
+            d="M6 9L12 15L18 9"
             stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
@@ -26,7 +26,7 @@ const WrapperArrow = ({ isOpen }) => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="M6 9L12 15L18 9"
+            d="M15 18L9 12L15 6"
             stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
