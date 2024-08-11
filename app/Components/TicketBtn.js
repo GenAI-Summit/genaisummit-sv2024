46fe714bd78df3db03df9b89ccf035a6a7360332
Daniel777y <yudexu123@126.com>
2024-08-10 19:25:27 -0700

    fix: unite the width of buttons in banner

--- app/Components/TicketBtn.js
+++ app/Components/TicketBtn.js
@@ -1,12 +1,15 @@
 import Link from "next/link";
 
-const TicketBtn = () => {
+const TicketBtn = ({ width }) => {
   return (
     <Link
       href="https://whova.com/portal/registration/genai_202405/"
       target="_blank"
     >
-      <button className="bg-color1 hover:bg-color2 text-color7 font-bold py-2 px-4 rounded ease-in-out duration-300">
+      <button
+        className={`
+          bg-color1 hover:bg-color2 text-color7 font-bold py-2 px-4 rounded ease-in-out duration-300 ${width ? width : ""}`}
+      >
         Buy Tickets
       </button>
     </Link>
