9de5912ef49b3b5d77843a0e394c5c08d7c2b55f
Daniel777y <yudexu123@126.com>
2024-08-11 10:54:51 -0700

    feat: drawer animations

--- app/Components/Drawer.js
+++ app/Components/Drawer.js
@@ -1,4 +1,5 @@
 import Close from "./SVG/Close";
+import DrawerEnter from "../Motions/DrawerEnter";
 
 const Drawer = ({ children, isDrawerOpen, closeDrawer }) => {
   return (
@@ -6,30 +7,31 @@ const Drawer = ({ children, isDrawerOpen, closeDrawer }) => {
       className={
         " fixed overflow-hidden bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out z-50 text-color5 " +
         (isDrawerOpen
-          ? " transition-opacity opacity-100 duration-500 translate-x-0 "
-          : " transition-all delay-500 opacity-0 translate-x-full ")
+          ? " opacity-100 translate-x-0 "
+          : " opacity-0 translate-x-full ")
       }
     >
-      <div
-        className={
-          " w-screen max-w-lg right-0 absolute bg-color7 h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  " +
-          (isDrawerOpen ? " translate-x-0 " : " translate-x-full ")
-        }
-      >
-        <div className="relative w-screen max-w-lg p-7 flex flex-col space-y-6 overflow-y-scroll h-full">
-          <button
-            className="fixed top-1 left-2 text-xl z-50"
-            onClick={closeDrawer}
-          >
-            <Close />
-          </button>
-          {children}
+      <DrawerEnter isOpen={isDrawerOpen}>
+        <div
+          className={
+            " w-screen max-w-lg right-0 absolute bg-color7 h-full shadow-xl transform  "
+          }
+        >
+          <div className="relative w-screen max-w-lg p-7 flex flex-col space-y-6 overflow-y-scroll h-full">
+            <button
+              className="fixed top-1 left-2 text-xl z-50"
+              onClick={closeDrawer}
+            >
+              <Close />
+            </button>
+            {children}
+          </div>
         </div>
-      </div>
-      <div
-        className=" w-screen h-full cursor-pointer "
-        onClick={closeDrawer}
-      ></div>
+        <div
+          className=" w-screen h-full cursor-pointer "
+          onClick={closeDrawer}
+        ></div>
+      </DrawerEnter>
     </div>
   );
 };
