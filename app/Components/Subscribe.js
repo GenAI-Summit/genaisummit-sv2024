164157d25714cefaa880ae865b17a85257e13e02
Daniel777y <yudexu123@126.com>
2024-08-10 17:58:49 -0700

    refactory: update subscription script

--- app/Components/Subscribe.js
+++ app/Components/Subscribe.js
@@ -1,23 +1,13 @@
 "use client";
 
 import { useEffect } from "react";
+import Script from "next/script";
 import SectionLayout from "../Layouts/SectionLayout";
 import CardLayout from "../Layouts/CardLayout";
 
 const Subscribe = () => {
   useEffect(() => {
     window._ctct_m = "30bb8cd15e4ce5c252b5c0df5da52b42";
-    const script = document.createElement("script");
-    script.id = "signupScript";
-    script.src =
-      "//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
   }, []);
 
   return (
@@ -30,6 +20,10 @@ const Subscribe = () => {
           className="ctct-inline-form"
           data-form-id="dbd60e74-0f3c-45cf-a70a-e7d88e062509"
         ></div>
+        <Script
+          src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js"
+          strategy="lazyOnload"
+        />
       </CardLayout>
     </SectionLayout>
   );
