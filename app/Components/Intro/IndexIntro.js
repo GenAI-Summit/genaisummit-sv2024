a0c259ab54c27572065e44917d43f636d026067a
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-14 21:07:52 -0700

    feat: update styles

--- app/Components/Intro/IndexIntro.js
+++ app/Components/Intro/IndexIntro.js
@@ -2,10 +2,10 @@ import Intro from "./Intro";
 import TicketBtn from "../Button/TicketBtn";
 
 const IndexIntro = () => {
-  const title = "Build the Future";
+  const title = "Define the Future";
   const paragraphs = [
-    "Join us in exploring the future possibilities at the Generative AI Summit Silicon Valley 2024. This third large-scale generative AI conference in Silicon Valley will be held from November 1 to November 3, 2024, at the Mission City Ballroom of the Santa Clara Convention Center. The event is hosted by GPT DAO, a community spontaneously formed by developers, entrepreneurs, and enthusiasts passionate about generative AI.",
-    "We are at the forefront of a new technological wave, with generative AI revealing a future of infinite possibilities. With over 5000 attendees, 300 startups, and more than 1000 developers, the summit brings together the world's most cutting-edge entrepreneurial teams and investment institutions to jointly explore the future of this field.",
+    "Join us in exploring the future possibilities at the GenAI Summit Silicon Valley 2024. This large-scale Generative AI conference will be held from November 1 to November 3, 2024, at the Santa Clara Convention Center. The event is hosted by GPT DAO, the largest GenAI event brand in the world.",
+    "We are at the forefront of a new AGI wave. With over 5000 total attendees, 200 speakers, 1500 startups, 2000 founders/CEOs, 20 international media outlets, and more than 3000 developers, the summit brings together the world's most cutting-edge entrepreneurial teams, corp decision makers, and investment institutions to jointly explore the future of this field.",
   ];
   const images = [
     "/images/propaganda/index_intro1.webp",
