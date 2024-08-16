c0d9c963cb89db4e5fa14066003cd57788056f99
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-15 17:06:51 -0700

    #18 feat: improve presence animations

--- app/Motions/DropDown.js
+++ app/Motions/DropDown.js
@@ -10,7 +10,7 @@ const DropDown = ({ children, isOpen }) => {
           initial={{ opacity: 0, height: 0 }}
           animate={{ opacity: 1, height: "auto" }}
           exit={{ opacity: 0, height: 0 }}
-          transition={{ duration: 0.5, type: "spring" }}
+          transition={{ duration: 0.7, type: "spring", ease: "linear" }}
           className="w-full h-full overflow-hidden"
         >
           {children}
