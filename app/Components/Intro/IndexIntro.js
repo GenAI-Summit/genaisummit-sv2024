050cb3c6916730b676d450d9ef141edc479c738a
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-16 10:28:44 -0700

    update: update intro info

--- app/Components/Intro/IndexIntro.js
+++ app/Components/Intro/IndexIntro.js
@@ -5,7 +5,7 @@ const IndexIntro = () => {
   const title = "Define the Future";
   const paragraphs = [
     "Join us in exploring the future possibilities at the GenAI Summit Silicon Valley 2024. This large-scale Generative AI conference will be held from November 1 to November 3, 2024, at the Santa Clara Convention Center. The event is hosted by GPT DAO, the largest GenAI event brand in the world.",
-    "We are at the forefront of a new AGI wave. With over 5000 total attendees, 200 speakers, 1500 startups, 2000 founders/CEOs, 20 international media outlets, and more than 3000 developers, the summit brings together the world's most cutting-edge entrepreneurial teams, corp decision makers, and investment institutions to jointly explore the future of this field.",
+    "We are at the forefront of a new AGI wave. With over 10,000 total attendees, 200 speakers, 1,500 startups, 2,000 founders/CEOs, 20 international media outlets, and more than 3,000 developers, the summit brings together the world's most cutting-edge entrepreneurial teams, corp decision makers, and investment institutions to jointly explore the future of this field.",
   ];
   const images = [
     "/images/propaganda/index_intro1.webp",
