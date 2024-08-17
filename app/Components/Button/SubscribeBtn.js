5b20bf3e23de6017f89311834576fed6f6b2832a
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-17 12:30:15 -0700

    update subscription btn color

--- app/Components/Button/SubscribeBtn.js
+++ app/Components/Button/SubscribeBtn.js
@@ -5,7 +5,7 @@ const SubscribeBtn = ({ width }) => {
     <Link href="/subscription" target="_blank">
       <button className={`relative group z-0 ${width}`}>
         <div className="absolute inset-0 -z-10 rounded-full bg-gradient-to-br from-color1 to-color2"></div>
-        <div className="m-[1.5px] rounded-full bg-color6 px-4 py-2 transition-colors duration-300 hover:bg-gray-700">
+        <div className="m-[1.5px] rounded-full bg-color8 px-4 py-2 transition-colors duration-300 hover:bg-gray-200">
           <span className="h-full w-full rounded-full bg-gradient-to-br from-color1 to-color2 bg-clip-text font-light text-transparent">
             Get Updates
           </span>
