fd589d106b942c6e7631e3738bac8ec67913acce
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-17 20:01:16 -0700

    update speakers & navbar & float footer styles

--- app/Components/Speakers.js
+++ app/Components/Speakers.js
@@ -16,8 +16,8 @@ const Speakers = ({ speakers }) => {
               className={`w-[30%] md:w-[18%] lg:w-[20%] ${styles.border}`}
             >
               <SpeakerCard key={speaker.id} speaker={speaker} />
-              <div className="mt-4 flex flex-col justify-center items-start text-color6 gap-y-4">
-                <p className="text-[8px] md:text-[12px] lg:text-[14px] font-bold">
+              <div className="mt-4 flex flex-col justify-center items-start text-color6 gap-y-2 md:gap-y-4">
+                <p className="text-[9px] md:text-[12px] lg:text-[14px] font-bold">
                   {speaker.name}
                 </p>
                 {speaker.title && (
