c70a856fcb69d5768a0a8ab972c594428c5c5920
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-17 23:43:25 -0700

    update styles

--- app/Components/FloatFooter.js
+++ app/Components/FloatFooter.js
@@ -18,7 +18,7 @@ const FloatFooter = () => {
   return (
     <div
       className={`
-    fixed bottom-4 w-[95%] text-color6 pt-[2.5px] bg-gradient-to-r from-color1 to-color2 shadow-lg rounded-lg md:hidden z-30 ${isVisible ? "opacity-100" : "opacity-0"}`}
+    fixed bottom-2 w-[95%] text-color6 pt-[2.5px] bg-gradient-to-r from-color1 to-color2 shadow-lg rounded-lg md:hidden z-30 ${isVisible ? "opacity-100" : "opacity-0"}`}
     >
       <div className="bg-color13 z-40 px-4 py-2 flex justify-between items-center">
         <span className="font-semibold">$99 – $1099</span>
