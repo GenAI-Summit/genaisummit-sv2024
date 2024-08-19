0eff2cc8cd88de31d8260bb3e2db5e4446d647b1
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-18 23:34:30 -0700

    update styles

--- app/Layouts/CardLayout.js
+++ app/Layouts/CardLayout.js
@@ -1,7 +1,7 @@
 const CardLayout = ({ children }) => {
   return (
-    <div className="w-full h-full bg-colorCardBg shadow-lg rounded-lg p-4">
-      {children}
+    <div className="w-full h-full pt-[2.5px] bg-gradient-to-r from-color1 to-color2 shadow-lg rounded-lg">
+      <div className="w-full h-full bg-colorCardBg p-4">{children}</div>
     </div>
   );
 };
