4dbe3c7d4d6024c610760f009153a68548be3dee
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-18 22:06:18 -0700

    fix typo

--- app/Components/Exhibitors.js
+++ app/Components/Exhibitors.js
@@ -3,7 +3,7 @@ import OrganizationCard from "../Components/OrganizationCard";
 
 const Exhibitors = ({ exhibitors }) => {
   return (
-    <SectionLayout title="Exhibitiors" description="MEET OUR EXHIBITIORS">
+    <SectionLayout title="Exhibitors" description="MEET OUR EXHIBITIORS">
       <div className="w-full flex flex-wrap mt-6 items-center gap-x-[4%] md:gap-x-[4%] lg:gap-x-[2%] gap-y-6 md:gap-y-8 lg:gap-y-4">
         {exhibitors.map((exhibitor) => (
           <div
