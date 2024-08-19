92c6fa51a6a066e82f44bd223a18332627d351e1
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-18 20:59:48 -0700

    feat: add industry leaders

---
+++ app/Components/IndexSections/Attendees.js
@@ -0,0 +1,35 @@
+"use client";
+
+import SectionLayout from "../../Layouts/SectionLayout";
+import Loader from "../Loader";
+import Error from "../Error";
+import useExhibitors from "../../Hooks/useExhibitors";
+import Logo from "../Logo";
+import Link from "next/link";
+
+const Attendees = () => {
+  const { attendees, isLoading, isError } = useExhibitors();
+  if (isLoading) return <Loader />;
+  if (isError) return <Error />;
+  return (
+    <SectionLayout
+      title="Industy Leaders"
+      description="THE WORLD'S LEADING COMPANIES ATTENDED GENAI SUMMIT, 29-31 MAY 2024."
+    >
+      <div className="mt-8 md:mt-10 flex flex-wrap justify-start items-center gap-y-7 md:gap-y-7 lg:gap-y-6 gap-x-[4%] md:gap-x-[4%] lg:gap-x-[2%]">
+        {attendees.map((media) => (
+          <Link
+            key={media.id}
+            className="w-[48%] h-36 md:w-[30%] lg:w-[23%] lg:h-27 bg-color13 cursor-pointer border-2 border-colorCardBorder ease-in-out duration-300 hover:border-colorCardBorderHover rounded-lg shadow-md"
+            href={media.url}
+            target="_blank"
+          >
+            <Logo src={media.logo} alt={media.name} />
+          </Link>
+        ))}
+      </div>
+    </SectionLayout>
+  );
+};
+
+export default Attendees;
