38f04068f417b3ab6730fedf90da9b8617e4a035
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-18 21:55:19 -0700

    sort attendees

--- app/Hooks/useExhibitors.js
+++ app/Hooks/useExhibitors.js
@@ -10,9 +10,22 @@ const useExhibitors = () => {
     fetcher,
   );
 
-  const attendees = data?.data.filter(
-    (exhibitor) => exhibitor.tier === "Attendee",
-  );
+  const attendees = data?.data
+    .filter((exhibitor) => exhibitor.tier === "Attendee")
+    .sort((a, b) => {
+      const rankRegex = /^_RANK_(\d+)_/;
+      const aMatch = a.name.match(rankRegex);
+      const bMatch = b.name.match(rankRegex);
+      if (aMatch && bMatch) {
+        return parseInt(aMatch[1]) - parseInt(bMatch[1]);
+      } else if (aMatch) {
+        return -1;
+      } else if (bMatch) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
   const exhibitors = data?.data.filter(
     (exhibitor) => exhibitor.tier !== "Attendee" && exhibitor.tier !== "Media",
   );
