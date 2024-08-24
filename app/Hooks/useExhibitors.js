13d71a04073e99a5de43992d8ebb77ad9039d539
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-24 16:50:57 -0700

    remove rank prefix

--- app/Hooks/useExhibitors.js
+++ app/Hooks/useExhibitors.js
@@ -10,50 +10,19 @@ const useExhibitors = () => {
     fetcher,
   );
 
-  const processedData = data?.data.sort(sorter).map((exhibitor) => {
-    return {
-      ...exhibitor,
-      name: exhibitor.name.replace(/^_RANK_\d+_/, ""),
-    };
-  });
+  const exhibitors = data?.data;
 
-  const attendees = processedData?.filter(
-    (exhibitor) => exhibitor.tier === "Attendee",
-  );
-  const exhibitors = processedData?.filter(
-    (exhibitor) => exhibitor.tier !== "Attendee" && exhibitor.tier !== "Media",
-  );
-  const media = processedData?.filter(
-    (exhibitor) => exhibitor.tier === "Media",
-  );
-  const sponsors = processedData?.filter((exhibitor) =>
+  const sponsors = exhibitors?.filter((exhibitor) =>
     sponsorTiers.includes(exhibitor.tier),
   );
 
   return {
-    attendees,
     exhibitors,
-    media,
     sponsors,
     sponsorTiers,
     isLoading,
     isError: error,
   };
-};
-
-const sorter = (a, b) => {
-  const rankRegex = /^_RANK_(\d+)_/;
-  const aMatch = a.name.match(rankRegex);
-  const bMatch = b.name.match(rankRegex);
-  if (aMatch && bMatch) {
-    return parseInt(aMatch[1]) - parseInt(bMatch[1]);
-  } else if (aMatch) {
-    return -1;
-  } else if (bMatch) {
-    return 1;
-  } else {
-    return 0;
-  }
 };
 
 export default useExhibitors;
