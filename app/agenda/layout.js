471da89096537142641ddc7dbf90fe0c46a68276
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-25 17:34:18 -0700

    update description

--- app/agenda/layout.js
+++ app/agenda/layout.js
@@ -1,5 +1,7 @@
 export const metadata = {
   title: "Agenda | GenAI Summit Silicon Valley 2024 | GPTDAO",
+  description:
+    "Explore cutting-edge GenAI topics over 3 days. From keynotes to panel discussions, dive deep into the latest advancements and applications.",
 };
 
 const AgendaLayout = ({ children }) => {
--- app/exhibitors/layout.js
+++ app/exhibitors/layout.js
@@ -1,9 +1,15 @@
 export const metadata = {
   title: "Exhibitors | GenAI Summit Silicon Valley 2024 | GPTDAO",
+  description:
+    "Engage with leading companies and startups showcasing groundbreaking GenAI solutions. Experience hands-on demos and network with industry disruptors.",
 };
 
 const ExhibitorsLayout = ({ children }) => {
-  return <div className="max-w-maxWidth w-full lg:w-3/4 h-full pt-20 px-5">{children}</div>;
+  return (
+    <div className="max-w-maxWidth w-full lg:w-3/4 h-full pt-20 px-5">
+      {children}
+    </div>
+  );
 };
 
 export default ExhibitorsLayout;
