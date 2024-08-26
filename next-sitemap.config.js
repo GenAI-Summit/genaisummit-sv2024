522592e73ed5f2fe5507259d246a5bbcf8ea0e45
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-25 19:13:49 -0700

    remove sessions sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,5 @@
-import { getSpeakers, getOrganizations, getSessions } from "./lib/api.js";
+// import { getSpeakers, getOrganizations, getSessions } from "./lib/api.js";
+import { getSpeakers, getOrganizations } from "./lib/api.js";
 
 const nextSitemapConfig = {
   siteUrl: "https://genaisummit.ai",
@@ -6,7 +7,7 @@ const nextSitemapConfig = {
   additionalPaths: async () => {
     const speakers = await getSpeakers();
     const organizations = await getOrganizations();
-    const sessions = await getSessions();
+    //const sessions = await getSessions();
 
     const speakerPaths = speakers.map((speaker) => ({
       loc: `/speaker/${speaker.id}`,
@@ -22,14 +23,17 @@ const nextSitemapConfig = {
       priority: 0.5,
     }));
 
+    /*
     const sessionPaths = sessions.map((session) => ({
       loc: `/session/${session.id}`,
       lastmod: new Date().toISOString(),
       changefreq: "daily",
       priority: 0.5,
     }));
+    */
 
-    return [...speakerPaths, ...organizationPaths, ...sessionPaths];
+    //return [...speakerPaths, ...organizationPaths, ...sessionPaths];
+    return [...speakerPaths, ...organizationPaths];
   },
   robotsTxtOptions: {
     policies: [
