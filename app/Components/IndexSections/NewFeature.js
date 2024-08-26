60952065dea0afcc0f6da260bd492802a6a56272
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-25 20:57:18 -0700

    update new features styles

--- app/Components/IndexSections/NewFeature.js
+++ app/Components/IndexSections/NewFeature.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const NewFeature = ({ feature, index }) => {
   return (
     <div
-      className={`w-full flex items-start md:items-center justify-between gap-8 ${
+      className={`w-full flex items-center justify-between gap-8 ${
         index % 2 === 0 ? "flex-row" : "flex-row-reverse"
       }`}
     >
@@ -16,8 +16,8 @@ const NewFeature = ({ feature, index }) => {
         objectFit="cover"
       />
       <div className="p-4">
-        <h2 className="text-2xl font-bold">{feature.title}</h2>
-        <p className="mt-4 text-xl">{feature.description}</p>
+        <h2 className="text-lg md:text-2xl font-bold">{feature.title}</h2>
+        <p className="mt-4 md:text-xl">{feature.description}</p>
       </div>
     </div>
   );
