ab52508505d3111b49c460885cc52303b94064cd
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-25 17:24:29 -0700

    add additional sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 const nextSitemapConfig = {
   siteUrl: "https://genaisummit.ai",
   generateRobotsTxt: true,
+  additionalSitemaps: ["https://api.gptdao.ai/sitemap/sitemap.xml"],
 };
 
 export default nextSitemapConfig;
