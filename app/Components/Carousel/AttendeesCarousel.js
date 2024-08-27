d9f38645e1f74145d71cc1e85cf809db4db75b80
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-08-26 18:31:04 -0700

    add notable attendees images

---
+++ app/Components/Carousel/AttendeesCarousel.js
@@ -0,0 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Logo from "../Logo";
+
+const AttendeesCarousel = ({ attendeesArray }) => {
+  const [index, setIndex] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  useEffect(() => {
+    if (!attendeesArray) return;
+
+    const timer = setInterval(() => {
+      setIsTransitioning(true);
+      setTimeout(() => {
+        setIndex((prev) => (prev === attendeesArray.length - 1 ? 0 : prev + 1));
+        setIsTransitioning(false);
+      }, 1000);
+    }, 10000);
+
+    return () => clearInterval(timer);
+  }, [attendeesArray]);
+
+  if (!attendeesArray) {
+    return null;
+  }
+
+  return (
+    <>
+      {attendeesArray[index].map((item) => (
+        <Link
+          key={item.id}
+          className={`w-[23%] h-24 md:w-[23%] md:h-28 lg:w-[15%] lg:h-32 bg-theme1Gray3 cursor-pointer border-2 border-theme1Gray3 ease-in-out duration-300 hover:border-theme1Gray2 rounded-lg shadow-md transition-opacity duration-1000 ${isTransitioning ? "opacity-0" : "opacity-100"}`}
+          href={item.url}
+          target="_blank"
+        >
+          <Logo src={item.logo} alt={item.name} />
+        </Link>
+      ))}
+    </>
+  );
+};
+
+export default AttendeesCarousel;
--- app/Components/IndexSections/Attendees.js
+++ app/Components/IndexSections/Attendees.js
@@ -1,26 +1,16 @@
 import SectionLayout from "../../Layouts/SectionLayout";
-import Logo from "../Logo";
-import Link from "next/link";
 import { getNotableAttendees } from "../../../lib/api";
+import AttendeesCarousel from "../Carousel/AttendeesCarousel";
 
 const Attendees = async () => {
-  const attendees = await getAttendeesData();
+  const attendeesArray = await getAttendeesData();
   return (
     <SectionLayout
       title="Notable Attendees"
       description="THE WORLD'S LEADING COMPANIES ATTENDED GENAI SUMMIT, 29-31 MAY 2024."
     >
       <div className="mt-8 md:mt-10 flex flex-wrap justify-start items-center gap-y-3 md:gap-y-4 lg:gap-y-4 gap-x-[2%] md:gap-x-[2%] lg:gap-x-[1.5%]">
-        {attendees.map((media) => (
-          <Link
-            key={media.id}
-            className="w-[23%] h-24 md:w-[23%] md:h-28 lg:w-[15%] lg:h-32 bg-theme1Gray3 cursor-pointer border-2 border-theme1Gray3 ease-in-out duration-300 hover:border-theme1Gray2 rounded-lg shadow-md"
-            href={media.url}
-            target="_blank"
-          >
-            <Logo src={media.logo} alt={media.name} />
-          </Link>
-        ))}
+        <AttendeesCarousel attendeesArray={attendeesArray} />
       </div>
     </SectionLayout>
   );
