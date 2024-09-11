5305c4b633366bdd75fcd9c2480cc0006f3caacc
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-09-11 11:54:50 -0700

    update edu verification

--- components/Button/EduSummit.js
+++ components/Button/EduSummit.js
@@ -1,10 +1,10 @@
-const EduSummit = ({ onSubmit }) => {
+const EduSummit = ({ onSubmit, cooldown }) => {
   return (
-    <button className="w-48 group relative z-0" onClick={onSubmit}>
+    <button className="w-64 group relative z-0" onClick={onSubmit} disabled={cooldown > 0}>
       <div className="absolute inset-0 -z-10 rounded-full bg-gradient-to-br from-theme1Color1 via-theme1Color2 to-theme1Color3"></div>
       <div className="m-[1.5px] rounded-full bg-theme1Dark1 px-4 py-2 transition duration-300 group-hover:bg-gray-800">
         <span className="h-full w-full rounded-full bg-gradient-to-br from-theme1Color1 via-theme1Color2 to-theme1Color3 bg-clip-text font-light text-transparent">
-          Verify
+          {cooldown > 0 ? `Resend in ${cooldown} seconds` : "Verify"}
         </span>
       </div>
     </button>
