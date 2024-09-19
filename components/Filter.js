db4ab2198806bea0efbccf4a686b067578604b6a
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-09-19 11:34:59 -0700

    improve filter wrapper styles

--- components/Filter.js
+++ components/Filter.js
@@ -32,18 +32,23 @@ const Filter = ({ name, options, selected, onSelect }) => {
             initial={{ opacity: 0, height: 0 }}
             animate={{ opacity: 1, height: "auto" }}
             exit={{ opacity: 0, height: 0 }}
-            transition={{ duration: 1.0, type: "spring", ease: "linear" }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
           >
-            {options.map((option, index) => {
-              return (
-                <div key={index} onClick={() => onSelect(option)}>
-                  <CheckBox
-                    checked={selected.includes(option)}
-                    label={option}
-                  />
-                </div>
-              );
-            })}
+            {options.map((option, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.2, delay: index * 0.05 }}
+                onClick={() => onSelect(option)}
+              >
+                <CheckBox
+                  checked={selected.includes(option)}
+                  label={option}
+                />
+              </motion.div>
+            ))}
           </motion.div>
         )}
       </AnimatePresence>
