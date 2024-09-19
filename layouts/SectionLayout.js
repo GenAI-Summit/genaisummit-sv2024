1953c1b0b323b1860f18fd334fd83941aac309b9
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-09-19 16:48:41 -0700

    fix font size

--- layouts/SectionLayout.js
+++ layouts/SectionLayout.js
@@ -6,7 +6,7 @@ const SectionLayout = ({ title, description, children, widget }) => {
           <div>
             <div className="bg-gradient-to-r from-theme1Color1 via-theme1Color2 to-theme1Color3 w-10 h-1" />
             <div className="w-full mt-6">
-              <span className="text-3xl font-monaspace_neon bg-gradient-to-br from-theme1Color1 via-theme1Color2 to-theme1Color3 bg-clip-text text-transparent">
+              <span className="text-2xl font-monaspace_neon bg-gradient-to-br from-theme1Color1 via-theme1Color2 to-theme1Color3 bg-clip-text text-transparent">
                 {title}
               </span>
             </div>
@@ -17,14 +17,14 @@ const SectionLayout = ({ title, description, children, widget }) => {
         <>
           <div className="bg-gradient-to-r from-theme1Color1 via-theme1Color2 to-theme1Color3 w-10 h-1" />
           <div className="w-full mt-6">
-            <span className="text-3xl font-monaspace_neon bg-gradient-to-br from-theme1Color1 via-theme1Color2 to-theme1Color3 bg-clip-text text-transparent">
+            <span className="text-2xl font-monaspace_neon bg-gradient-to-br from-theme1Color1 via-theme1Color2 to-theme1Color3 bg-clip-text text-transparent">
               {title}
             </span>
           </div>
         </>
       )}
       {description && (
-        <div className="mt-6 text-theme1Color2">{description}</div>
+        <div className="mt-6 text-theme1Color2 text-sm md:text-base">{description}</div>
       )}
       <div className="flex flex-col items-center justify-center gap-y-10 w-full h-full">
         {children}
