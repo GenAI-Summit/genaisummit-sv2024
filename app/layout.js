0bffd3b602191dae3424febd63b23e7753d257c0
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-09-23 10:41:15 -0700

    pixel base code

--- app/layout.js
+++ app/layout.js
@@ -136,6 +136,33 @@ const RootLayout = ({ children }) => {
           src="https://chatbox.copilot.livex.ai/livex.min.js"
           strategy="afterInteractive"
         />
+        {/*<!-- Meta Pixel Code -->*/}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+        >
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '493023690363001'); 
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=493023690363001&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/*<!-- End Meta Pixel Code -->*/}
       </body>
       <GoogleAnalytics gaId="G-6ETB3L7DWM" />
     </html>
