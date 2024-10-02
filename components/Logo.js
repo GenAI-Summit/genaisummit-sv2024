3f99513852794deb385b6194295169774ab07a44
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-10-01 17:08:56 -0700

    improve logo padding

--- components/Logo.js
+++ components/Logo.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
-const Logo = ({ src, alt, width = 200, height = 200 }) => {
+const Logo = ({ src, alt, width = 200, height = 200, padding = "p-4" }) => {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center p-4 overflow-hidden">
+    <div className={`w-full h-full flex flex-col justify-center items-center overflow-hidden ${padding}`}>
       <Image src={src} alt={alt} width={width} height={height} loading="lazy" />
     </div>
   );
