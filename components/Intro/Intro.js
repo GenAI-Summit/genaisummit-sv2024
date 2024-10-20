1fcf421ea02b43b3b1199920d343adaa141d4efd
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-10-20 00:04:29 -0700

    update intro layout

--- components/Intro/Intro.js
+++ components/Intro/Intro.js
@@ -6,15 +6,15 @@ import IntroCarousel from "@/components/Carousel/IntroCarousel";
 const Intro = ({ title, paragraphs, images, widget, imgAspect }) => {
   return (
     <SectionLayout title={title} widget={widget}>
-      <div className="w-full flex flex-col 2xl:flex-row items-start justify-between gap-y-8 pt-8">
-        <div className="w-full 2xl:w-[48%] flex flex-col gap-y-6">
+      <div className="w-full flex flex-col xl:flex-row lg:gap-x-[2%] items-start justify-between gap-y-8 pt-8">
+        <div className="w-full xl:w-[48%] flex flex-col gap-y-6">
           {paragraphs.map((paragraph, index) => (
             <p key={index} className="text-base md:text-lg">
               {paragraph}
             </p>
           ))}
         </div>
-        <div className="w-full 2xl:w-[48%] 2xl:max-w-[700px]">
+        <div className="w-full xl:w-[48%] xl:max-w-[700px]">
           <Suspense fallback={<Loader />}>
             <IntroCarousel images={images} imgAspect={imgAspect} />
           </Suspense>
