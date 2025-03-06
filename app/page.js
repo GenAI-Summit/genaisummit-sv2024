e4ea124ad83c08b0aa9db65fc317681b2a881ff1
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-03-05 18:39:32 -0800

    remove popup

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ import NewFeatures from "@/components/IndexSections/NewFeatures";
 import Tickets from "@/components/Tickets";
 import SectionEnter from "@/motions/SectionEnter";
 // import NewlyUpdateModal from "@/components/IndexSections/NewlyUpdateModal";
-import PopupModal from "@/components/IndexSections/PopupModal";
+// import PopupModal from "@/components/IndexSections/PopupModal";
 import IndexAgenda from "@/components/IndexSections/IndexAgenda";
 import IndexSponsors from "@/components/IndexSections/IndexSponsors";
 // import TeslaIntro from "@/components/Intro/TeslaIntro";
@@ -23,9 +23,11 @@ export const revalidate = 60;
 const Index = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center gap-y-12 md:gap-y-16">
+      {/*
       <Suspense fallback={null}>
         <PopupModal />
       </Suspense>
+      */}
       <Banner />
       <div className="max-w-maxWidth w-full lg:w-5/6 px-5 flex flex-col gap-y-12 md:gap-y-16">
         <IndexSponsors />
