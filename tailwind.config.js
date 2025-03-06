e970d1a87c9808801db5e4b0b90fbd09dcf0b34e
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-03-05 18:52:54 -0800

    update js settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -177,3 +177,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+export default config;
